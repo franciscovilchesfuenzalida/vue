8b5375d1f1205eceb4dd107bc38ebb46b99da4ae
Francisco <franciscovilchesf@gmail.com>
2021-05-23 02:03:10 -0400

    Routes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+const Foo = { template: '<div>foo</div>'}
+const Bar = { template: '<div>bar</div>'}
+
+const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar },
+];
+
+const router = new VueRouter({
+    routes
+});
+
 const app = new Vue({
     el: '#app',
+    router,
     data: {
         message: 'Hola',
         suma: 3 + 2,
